0bbdc9c4dfd743a64f20d539011aa48bd72cfcbb
LAPTOP-GJIF7K83\sanek <sanek33333@gmail.com>
2025-01-19 22:06:04 +0100

    adding catch2 for testing. Also preparing json_parser executable and making project structure more sane

---
+++ JSONParser/json_parser_main.cpp
@@ -0,0 +1,6 @@
+#include "json_parser.cpp"
+
+int main()
+{
+    return 0;
+}
---
+++ test/test_json_parser.cpp
@@ -0,0 +1,16 @@
+//
+// Created by sanek on 19/01/2025.
+//
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+#include "../JSONParser/json_parser.cpp"
+
+TEST_CASE("EMPTY TEST")
+{
+  REQUIRE(1 == 1);
+}
+
+TEST_CASE("ANOTHER TEST")
+{
+    REQUIRE(2 == 2);
+}
